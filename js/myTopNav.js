e34d6e9cab2a3420b4f3a79024b0980795e2e2d5
Leon Darko <57622023+thah-leon@users.noreply.github.com>
2020-11-25 12:02:12 +0000

    Initial upload of Jambo-choptime
    :)

---
+++ js/myTopNav.js
@@ -0,0 +1,10 @@
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.className = "topnav" + " w3-card" + " w3-animate-top" + " w3-light-blue";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-light-blue", "");
+    }
+}
